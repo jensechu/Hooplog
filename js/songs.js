3380f873f3b5a8e883051f8fe3baedf5b98606db
Jensen Hussey <jensechu@gmail.com>
2012-01-15 07:26:09 -0500

    added a random song button

--- js/songs.js
+++ js/songs.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
-
+function random_song(){
     $.getJSON("/data/songs.json", function(data) {
 	var songs = data.songs;
 	
 	var random = songs[Math.floor(Math.random() * songs.length)];
-
+	
 	console.log(random);
-
+	
 	$(".spotlight .song-url").attr("href", random.url);
 	$(".spotlight .song-title").text(random.title);
 	$(".spotlight .song-artist").text(random.artist);
-
-	});
-
-
+	    
     });
+    return false;
+}
+
+$(document).ready(function(){
+    random_song();
+
+    $("#random-song").click(random_song);
+
+});
